fe78d7bb6f370df8c0b0dc8e8a33dfd3441af65a
tao-z141 <taozaddi@gmail.com>
2026-06-17 10:12:11 +0200

    Update silver_to_gold.py

--- glue_jobs/silver_to_gold.py
+++ glue_jobs/silver_to_gold.py
@@ -1,27 +1,42 @@
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
+from awsglue.job import Job
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 
-taxi = spark.read.parquet("s3://nyc-taxi-silver/taxi_clean/")
-weather = spark.read.parquet("s3://nyc-taxi-silver/weather_clean/")
+job = Job(glueContext)
+job.init("silver_to_gold", {})
+
+# ======================
+# READ SILVER
+# ======================
+taxi = spark.read.parquet("s3://nyc-taxi-platform/silver/taxi_clean/")
+weather = spark.read.parquet("s3://nyc-taxi-platform/silver/weather_clean/")
 
 taxi.createOrReplaceTempView("taxi")
-weather.createOrReplaceTempView("weather")
 
+# ======================
+# GOLD KPI
+# ======================
 gold = spark.sql("""
 SELECT
     DATE(tpep_pickup_datetime) as day,
     COUNT(*) as nb_trips,
-    AVG(trip_distance) as avg_distance
+    AVG(trip_distance) as avg_distance,
+    AVG(fare_amount) as avg_fare
 FROM taxi
 GROUP BY DATE(tpep_pickup_datetime)
 """)
 
+# ======================
+# WRITE GOLD
+# ======================
 gold.write.mode("overwrite").parquet(
-    "s3://nyc-taxi-gold/trips_kpi/"
+    "s3://nyc-taxi-platform/gold/trips_kpi/"
 )
+
+job.commit()
 
 print("Silver → Gold done")
