9e469896a39044282ebb00dd017a20c55284278d
tao-z141 <taozaddi@gmail.com>
2026-06-17 10:12:53 +0200

    Enhance job run function with error handling
    Added error handling for job execution and improved print statements.

--- glue_jobs/scheduler.py
+++ glue_jobs/scheduler.py
@@ -2,9 +2,14 @@ import boto3
 
 glue = boto3.client("glue")
 
+
 def run_job(job_name):
-    response = glue.start_job_run(JobName=job_name)
-    print("Job started:", response["JobRunId"])
+    try:
+        response = glue.start_job_run(JobName=job_name)
+        print(f"Job {job_name} started:", response["JobRunId"])
+    except Exception as e:
+        print(f"Error running {job_name}:", e)
+
 
 if __name__ == "__main__":
     run_job("bronze_to_silver")
