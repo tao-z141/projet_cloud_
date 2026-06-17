721cefdf1b4088d963011ac000ef3966117c12b5
tao-z141 <taozaddi@gmail.com>
2026-06-17 10:11:50 +0200

    Refactor bronze to silver job for new S3 paths

--- glue_jobs/bronze_to_silver.py
+++ glue_jobs/bronze_to_silver.py
@@ -1,24 +1,38 @@
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue.utils import getResolvedOptions
+import sys
 
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 
-taxi = spark.read.parquet("s3://nyc-taxi-bronze/taxi/")
-weather = spark.read.json("s3://nyc-taxi-bronze/weather/")
+job = Job(glueContext)
+job.init("bronze_to_silver", {})
 
-# nettoyage basique
+# ======================
+# READ BRONZE (1 BUCKET)
+# ======================
+taxi = spark.read.parquet("s3://nyc-taxi-platform/bronze/taxi/")
+weather = spark.read.json("s3://nyc-taxi-platform/bronze/weather/")
+
+# ======================
+# CLEANING
+# ======================
 taxi_clean = taxi.dropna()
 
-# write silver
+# ======================
+# WRITE SILVER
+# ======================
 taxi_clean.write.mode("overwrite").parquet(
-    "s3://nyc-taxi-silver/taxi_clean/"
+    "s3://nyc-taxi-platform/silver/taxi_clean/"
 )
 
 weather.write.mode("overwrite").parquet(
-    "s3://nyc-taxi-silver/weather_clean/"
+    "s3://nyc-taxi-platform/silver/weather_clean/"
 )
+
+job.commit()
 
 print("Bronze → Silver done")
