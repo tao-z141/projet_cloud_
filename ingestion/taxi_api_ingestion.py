cbdfb4ced5d2d550cea4067411e187bc85a63467
tao-z141 <taozaddi@gmail.com>
2026-06-17 09:49:57 +0200

    Refactor taxi data ingestion and S3 upload process
    Updated S3 bucket name and improved data handling during upload.

--- ingestion/taxi_api_ingestion.py
+++ ingestion/taxi_api_ingestion.py
@@ -1,25 +1,41 @@
-import requests
 import pandas as pd
 import boto3
 from datetime import datetime
 
-S3_BUCKET = "nyc-taxi-bronze"
+# Bucket unique (Data Lake)
+S3_BUCKET = "nyc-taxi-platform"
 s3 = boto3.client("s3")
+
 
 def fetch_taxi_data():
     url = "https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet"
     df = pd.read_parquet(url)
+
+    # Petit nettoyage léger (important pour Silver ensuite)
+    df = df.dropna(subset=["tpep_pickup_datetime", "tpep_dropoff_datetime"])
+
     return df
 
+
 def upload_to_s3(df):
-    file_name = f"taxi_{datetime.now().strftime('%Y%m%d%H%M%S')}.parquet"
+
+    timestamp = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    file_name = f"taxi_{timestamp}.parquet"
 
     local_path = f"/tmp/{file_name}"
-    df.to_parquet(local_path)
 
-    s3.upload_file(local_path, S3_BUCKET, f"taxi/{file_name}")
+    # Sauvegarde locale
+    df.to_parquet(local_path, index=False)
 
-    print("Uploaded to S3 Bronze")
+    # Upload vers BRONZE (médaillon)
+    s3.upload_file(
+        local_path,
+        S3_BUCKET,
+        f"bronze/taxi/{file_name}"
+    )
+
+    print(f"Uploaded to s3://{S3_BUCKET}/bronze/taxi/{file_name}")
+
 
 if __name__ == "__main__":
     df = fetch_taxi_data()
